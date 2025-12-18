140612a39a21e70ed0a6c0a6b03ea16da118d7d7
Yash Pede <yashpedeofficial@gmail.com>
2025-12-18 23:25:49 +0530

    init

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@repo/ui/components/button";
+
+import "@repo/ui/styles/globals.css";
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <Button>hello</Button>
+    </div>
+  );
+}
+
+export default App;
