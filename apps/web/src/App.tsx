0c75096970bcb8840a86e7d01eac1a8d8efd2674
Yash Pede <yashpedeofficial@gmail.com>
2025-12-23 16:02:13 +0530

    update: auth gaurds

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,20 +1,16 @@
 import "@repo/ui/styles/globals.css";
-import { routeTree } from "./routeTree.gen";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { RouterProvider } from "@tanstack/react-router";
 import { ThemeProvider } from "@repo/ui/components/theme-provider";
 import { ClerkWrapper, useClerkAuth } from "./auth/clerk";
-const router = createRouter({
-  routeTree,
-});
+import { router } from "./router";
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
   }
 }
-
 function InnerApp() {
   const auth = useClerkAuth();
-
+  console.log("THE AUTH IS:", auth);
   if (auth.isLoading) {
     return (
       <div className="flex items-center justify-center min-h-screen">
